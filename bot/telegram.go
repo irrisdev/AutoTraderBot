39a134c226b52d2059b9206740aa3ae56d1bc95c
Irris <160921248+irrisdev@users.noreply.github.com>
2024-03-22 23:59:14 +0000

    Login via TELEGRAM_APITOKEN and wait till done

--- bot/telegram.go
+++ bot/telegram.go
@@ -2,17 +2,24 @@ package bot
 
 import (
 	telebot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/rs/zerolog/log"
 	"os"
+	"time"
 )
 
-func main() {
+func StartTelegramBot(done chan<- string) {
 
 	bot, err := telebot.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
 
 	if err != nil {
-		panic(err)
+
+		log.Err(err).Msg("Failed to start telegram bot")
+		return
 	}
 
-	bot.Debug = true
+	log.Info().Msg("Started telegram bot")
 
+	bot.Debug = true
+	time.Sleep(time.Second * 5)
+	done <- "Completed telegram"
 }
--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"github.com/irrisdev/AutoTraderBot/bot"
 	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"os"
 )
 
 // Initialising bots, set up scraping and schedules
 func main() {
+	//Setup logging
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
 	err := loadEnvVariables()
 	if err != nil {
 		log.Err(err).Msg("Failed to load environment variables")
 		return
 	}
+	log.Info().Msg("Loaded environment variables")
+
+	telegramStatus := make(chan string)
+
+	go bot.StartTelegramBot(telegramStatus)
+
+	log.Info().Msg(<-telegramStatus)
 
 }
 
