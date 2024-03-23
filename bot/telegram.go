781b51d85a50f4887e2db0bb8dd5fba4bf927c55
Irris <160921248+irrisdev@users.noreply.github.com>
2024-03-23 00:58:14 +0000

    Added command handler

--- bot/telegram.go
+++ bot/telegram.go
@@ -20,6 +20,49 @@ func StartTelegramBot(done chan<- string) {
 	log.Info().Msg("Started telegram bot")
 
 	bot.Debug = true
+	log.Info().Msg("Authorized on account " + bot.Self.UserName)
+
+	updateConfig := telebot.NewUpdate(0)
+	updateConfig.Timeout = 30
+
+	// Start polling Telegram for updates.
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+
+		//Discard empty messages as if no updates are detected in 30 seconds telegram will respond to request with empty struct
+		if update.Message == nil {
+			continue
+		}
+		if !update.Message.IsCommand() {
+			continue
+		}
+
+		//fmt.Println(update.Message.Text)
+
+		//msg := telebot.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		//msg.ReplyToMessageID = update.Message.MessageID
+
+		msg := telebot.NewMessage(update.Message.Chat.ID, "")
+
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "I understand /start and /stop"
+		case "start":
+			msg.Text = "Make sure this is a channel"
+		case "stop":
+			msg.Text = "Stopping update in {channelname}"
+		case "stopserver":
+			done <- "Completed telegram"
+		default:
+			msg.Text = "Not a command"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Err(err).Msg("An error occured while sending a message")
+		}
+
+	}
+
 	time.Sleep(time.Second * 5)
-	done <- "Completed telegram"
 }
