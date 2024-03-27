f1144dc1e7f36933af3d146e625f5a3721745550
irris <160921248+irrisdev@users.noreply.github.com>
2024-03-27 23:54:21 +0000

    Messy implementation of Scrape All Models
    Signed-off-by: irris <160921248+irrisdev@users.noreply.github.com>

--- bot/utils.go
+++ bot/utils.go
@@ -9,12 +9,15 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/rs/zerolog/log"
 	"io"
+	"math/rand/v2"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 	"time"
 )
 
-func ScrapeMakes() {
+func ScrapeMakes() []string {
 
 	var makes []string
 
@@ -49,7 +52,9 @@ func ScrapeMakes() {
 		makes = append(makes, value)
 	})
 
-	insertMakes(makes)
+	return makes
+
+	//insertMakes(makes)
 }
 
 func ScrapeModels(session *UserSession, progress chan<- string, result chan<- []string) {
@@ -68,10 +73,10 @@ func ScrapeModels(session *UserSession, progress chan<- string, result chan<- []
 	progress <- "No Existing models... Scraping off AutoTrader"
 	log.Info().Msg("Begging Scrape of model")
 	newReq := session.RequestDetails
-	url := createURL(&newReq)
-	log.Info().Msg(url)
+	urlS := createURL(&newReq)
+	log.Info().Msg(urlS)
 
-	go ScrapeModelURL(url, progress, result)
+	go ScrapeModelURL(urlS, progress, result, manufacture)
 }
 
 func getModels(manufacture string) ([]string, bool) {
@@ -112,7 +117,7 @@ func getModels(manufacture string) ([]string, bool) {
 	return modelsSlice, true
 }
 
-func ScrapeModelURL(url string, progress chan<- string, result chan<- []string) {
+func ScrapeModelURL(url string, progress chan<- string, result chan<- []string, manufacture string) {
 	log.Debug().Msg("Scraping Models")
 
 	browser := rod.New().Timeout(time.Minute).MustConnect()
@@ -162,14 +167,13 @@ func ScrapeModelURL(url string, progress chan<- string, result chan<- []string) 
 		modelsSlice = append(modelsSlice, text)
 	})
 
-	insertModels(modelsSlice, "BMW")
+	insertModels(modelsSlice, manufacture)
 
 	log.Debug().Msg("Completed Models Scrape")
 
 	defer browser.MustClose()
 	close(progress)
 	result <- modelsSlice
-	close(result)
 }
 
 func insertModels(sliceModel []string, vmake string) {
@@ -186,7 +190,7 @@ func insertModels(sliceModel []string, vmake string) {
 
 	for _, m := range sliceModel {
 
-		_, err := stmt.Exec(m, vmake)
+		_, err = stmt.Exec(m, vmake)
 		if err != nil {
 			log.Fatal().Err(err).Msg("Error while executing statement")
 		}
@@ -246,4 +250,113 @@ func startTx(db *sql.DB) *sql.Tx {
 		log.Fatal().Err(err).Msg("Error while beginning tx")
 	}
 	return tx
+}
+
+func InsertAll(carMakes []string, wg *sync.WaitGroup) {
+	pst := Postcodes[rand.IntN(len(Postcodes))]
+
+	browser := rod.New().Timeout(time.Minute * 5).MustConnect()
+
+	page := stealth.MustPage(browser).Timeout(time.Minute * 5)
+
+	page.MustNavigate("https://www.autotrader.co.uk/").MustWindowFullscreen()
+
+	page.MustWaitStable()
+	iframe := page.MustWaitLoad().MustElement(`#sp_message_container_1086457 iframe`)
+	if iframe == nil {
+		log.Debug().Msg("Accept cookies iframe not found")
+	} else {
+		log.Debug().Msg("Accept cookies iframe found")
+
+		// Switch to the iframe context
+		temppage := iframe.MustFrame()
+
+		// Click the reject button using its XPath
+		reject := temppage.MustElementX(`/html/body/div/div[2]/div[4]/button[2]`)
+		log.Debug().Msg("Reject button found")
+
+		// Wait for the reject button to be clickable
+		reject.MustWaitVisible().MustWaitEnabled().MustClick()
+		log.Debug().Msg("Reject button clicked")
+
+		temppage.MustWaitStable()
+
+	}
+
+	for _, m := range carMakes {
+		log.Info().Msg(fmt.Sprintf("Getting %s", m))
+		params := url.Values{}
+		params.Set("postcode", pst)
+		params.Set("make", m)
+		urlS := fmt.Sprintf("https://www.autotrader.co.uk/car-search?%s", params.Encode())
+		log.Info().Msg(urlS)
+		page.Timeout(time.Minute * 5)
+		page.MustNavigate(urlS)
+		page.MustWaitStable()
+
+		modelsBtn := page.MustElement(`#content > article > div.at__sc-1okmyrd-3.caRFaz > section > section:nth-child(3) > button`)
+
+		//fmt.Println(modelsBtn.HTML())
+		if modelsBtn == nil || isDisabled(modelsBtn) {
+			continue
+		}
+		modelsBtn.MustWaitStable().MustClick()
+
+		models := page.MustElement(`#model-facet-panel`).MustWaitStable()
+		html, err := models.HTML()
+		if err != nil {
+			log.Err(err)
+		}
+		doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+		if err != nil {
+			log.Err(err)
+		}
+
+		var modelsSlice []string
+		db := openDb()
+		defer db.Close()
+		query := "SELECT EXISTS(SELECT 1 FROM models WHERE model = '%s' AND make = '%s')"
+
+		doc.Find(".at__sc-1n64n0d-9.at__sc-qzn93z-3.bWawNd.eXNMEw").Each(func(i int, selection *goquery.Selection) {
+			text := selection.Text()
+
+			sqlStatement := fmt.Sprintf(query, text, m)
+			fmt.Println("SQL statement:", sqlStatement)
+
+			var exists int
+			err = db.QueryRow(sqlStatement).Scan(&exists)
+			if err != nil {
+				log.Err(err)
+			}
+			fmt.Println(exists)
+
+			if exists == 1 {
+				log.Info().Msg("Already exists... Skipping")
+				return
+			}
+
+			modelsSlice = append(modelsSlice, text)
+		})
+
+		log.Info().Msg(fmt.Sprintf("Results of %s %v", m, modelsSlice))
+
+		fmt.Println(modelsSlice)
+		insertModels(modelsSlice, m)
+
+	}
+
+	defer browser.MustClose()
+	wg.Done()
+}
+
+func isDisabled(element *rod.Element) bool {
+	attr, _ := element.Attribute("disabled")
+
+	newString := fmt.Sprintf("%V", attr)
+
+	if !strings.Contains(newString, "nil") {
+		log.Info().Msg("button disabled")
+		return true
+	}
+	return false
 }
--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
+	"sync"
 )
 
 func main() {
@@ -22,11 +23,23 @@ func main() {
 	}
 	log.Info().Msg("Loaded environment variables")
 
-	status := make(chan string)
+	//status := make(chan string)
 
-	go bot.TeleBot(status)
+	//go bot.TeleBot(status)
 	//bot.ScrapeModelTest()
 	//bot.ScrapeMakes()
-	log.Info().Msg(<-status)
+	makes := bot.ScrapeMakes()
+	var wg sync.WaitGroup
+	for i := 0; i < len(makes)-1; i += 20 {
+		wg.Add(1)
+		newSlice := makes[i : i+19]
+		go bot.InsertAll(newSlice, &wg)
+
+	}
+	wg.Wait()
+	//go bot.InsertAll(makes)
+	//go bot.InsertAll(makes)
+	//go bot.InsertAll(makes)
+	log.Info().Msg("Fully Finishes")
 
 }
